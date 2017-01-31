c5d5a9fff084e077f97d91e56f1694a0d40f55c3
ei17ringo <ei17ringo@gmail.com>
2017-01-31 08:01:27 +0800

    commit init

--- sampleGestureForDog/ViewController.swift
+++ sampleGestureForDog/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var gestureImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func dogTapped(_ sender: Any) {
+        //部品のアラートを作る
+        let alertController = UIAlertController(title: "コロからのメッセージ", message: "触った？", preferredStyle: .alert)
+        
+        //OKボタンを追加 handler...ボタンが押された時発動する処理を記述
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: {action in print("OK")}))
+        
+        //アラートを表示する
+        present(alertController,animated: true, completion: nil)
+    }
+    
+    @IBAction func dogSwiped(_ sender: Any) {
+        //部品のアラートを作る
+        let alertController = UIAlertController(title: "コロからのメッセージ", message: "なでなで", preferredStyle: .alert)
+        
+        //OKボタンを追加 handler...ボタンが押された時発動する処理を記述
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: {action in print("OK")}))
+        
+        //アラートを表示する
+        present(alertController,animated: true, completion: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
